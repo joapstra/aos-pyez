08f6be9e2ec1fe1956347deba365edf49aaa46e7
Jermey Schulman <jeremy@apstrktr.com>
2016-10-14 21:36:42 -0400

    bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ packages = find_packages(libdir)
 setup(
     name="aos-pyez",
     namespace_packages=['apstra'],
-    version="0.2.0",
+    version="0.3.0",
     author="Jeremy Schulman",
     author_email="jeremy@apstra.com",
     description=("Python wrapper library for Apstra AOS REST API "),
